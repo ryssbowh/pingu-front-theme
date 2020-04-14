80fcf3984218be34ef7ccb9bcef1f702860206c2
Boris Blondin <blondin.boris@gmx.com>
2020-04-14 07:05:08 +0100

    tweaks

--- views/errors/404.blade.php
+++ views/errors/404.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center text-center">
         <div class="col-md-8">
             <h1>404</h1>
-            <h2>{{ $exception->getMessage() }}</h2>
+            <h2>This page doesn't exist, byyyye</h2>
         </div>
     </div>
 </div>
--- views/modules/page/page.blade.php
+++ views/modules/page/page.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 	<div class="container">
-		<div class="{{ $classes }}">
+		<div class="{{ $classes->toHtml() }}">
 			@foreach($blocks as $block)
 				{!! $block->render() !!}
 			@endforeach
